402828e5080c31a71f8fb16ea280a1758a552c2e
JC Jiang <jcjiang@phys.ntu.edu.tw>
2026-04-01 12:09:17 +0800

    bump version to v0.2.1 and update docs

--- src/qkan/solver/cutile.py
+++ src/qkan/solver/cutile.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024, Jiun-Cheng Jiang. All rights reserved.
+# Copyright (c) 2026, Jiun-Cheng Jiang. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
